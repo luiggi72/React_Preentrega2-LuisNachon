bb7a495cf13f3531178cc47536b901d5726ab15b
Luis <luis.nachon@hotmail.com>
2023-05-21 14:19:00 -0600

    Actualizacion 5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import './App.css';
-import ShoppingCart from './components/ShoppingCart';
-
+import Contador from "./components/Contador";
+import ContadorMejorado from "./components/ContadorMejorado";
+import CrudApi from "./components/CrudApi";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
-  return(
+  return (
     <div>
-    
+      <h1>useReducer</h1>
+      <hr />
+      <CrudApi />
+      <hr />
       <ShoppingCart />
-  
+      <hr />
+      <ContadorMejorado />
+      <hr />
+      <Contador />
     </div>
   );
-};
+}
 
 export default App;
